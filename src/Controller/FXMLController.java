6437f2e806e57046faa43c6bad1a733083ae7aa0
marim.gabi <gabimarim06@gmail.com>
2018-06-03 11:49:22 -0300

    Telinha com botãozinhos

--- src/Controller/FXMLController.java
+++ src/Controller/FXMLController.java
@@ -5,9 +5,14 @@
  */
 package Controller;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -16,12 +21,48 @@ import javafx.fxml.Initializable;
  */
 public class FXMLController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Canvas canvas1;
+    
+    @FXML
+    private Canvas canvas2;
+    
+    @FXML
+    private Canvas canvas3;
+    
+    @FXML
+    private Canvas canvas4;
+    
+    @FXML
+    private Button botaoSeleciona;
+    
+    @FXML
+    private Button botaoExclui;
+    
+    @FXML
+    private Button botaoPonto;
+    
+    @FXML
+    private Button botaoCirculo;
+    
+    @FXML
+    private Button botaoReta;
+    
+    @FXML
+    private Button botaoPoligonoR;
+    
+    GraphicsContext gc1;
+    GraphicsContext gc2;
+    GraphicsContext gc3;
+    GraphicsContext gc4;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        gc1= canvas1.getGraphicsContext2D();
+        gc2= canvas2.getGraphicsContext2D();
+        gc3= canvas3.getGraphicsContext2D();
+        gc4= canvas4.getGraphicsContext2D();
     }    
     
 }
