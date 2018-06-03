ddd7462376bd42c6a95a9ad026c81671c8c238c4
marim.gabi <gabimarim06@gmail.com>
2018-06-03 07:15:24 -0300

    Telinha inicial, mas inicial msm

--- src/View/Main.java
+++ src/View/Main.java
@@ -10,9 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent p= FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+        Scene scene= new Scene(p);
+        
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Trabalho de CG");
         primaryStage.show();
     }
 
