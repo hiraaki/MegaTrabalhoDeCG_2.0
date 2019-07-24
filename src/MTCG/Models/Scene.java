609716ecc7535a4178853811e17d6c001481d2d9
Hiraaki <mauricio_ishida@hotmail.com>
2019-07-24 16:39:20 -0400

    d

---
+++ src/MTCG/Models/Scene.java
@@ -0,0 +1,9 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class Scene {
+    ArrayList<Polygon> scene;
+    Vertex camera;
+    Vertex Range;
+}
--- src/MTCG/View/Main.java
+++ src/MTCG/View/Main.java
@@ -1,5 +1,4 @@
 package View;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
